f388e4c405ba747f02d172845bc7c1ad92d28746
SunnySparks <Josediazt6@outlook.com>
2021-09-28 17:44:01 -0500

    simplify update method

--- src/updates.js
+++ src/updates.js
@@ -1,15 +1,15 @@
 function statusToggler(box, list) {
   box.addEventListener('change', () => {
     const nextSibling = box.nextElementSibling.value;
-    for (let i = 0; i < list.length; i += 1) {
-      if (box.checked && nextSibling === list[i].description) {
-        list[i].completed = true;
+    list.forEach((element) => {
+      if (box.checked && nextSibling === element.description) {
+        element.completed = true;
         localStorage.setItem('pushing', JSON.stringify(list));
-      } else if (!box.checked && nextSibling === list[i].description) {
-        list[i].completed = false;
+      } else if (!box.checked && nextSibling === element.description) {
+        element.completed = false;
         localStorage.setItem('pushing', JSON.stringify(list));
       }
-    }
+    });
   });
 }
 
